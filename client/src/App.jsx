d049b520a9245a4d405b14b6a40710871a643359
thisismostafaxx_ <mh1546226@gmail.com>
2023-10-03 03:32:16 +0300

    Update App.jsx for the page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 
-import './App.css'
-
+//import { Navbar, Home, Footer, Services, Transactions, Welcome, Client, Contact } from "./components";
 const App =() => {
  
   return (
@@ -8,8 +7,21 @@ const App =() => {
        <h1 className="text-3xl font-bold underline">
           Real Estate Block Chain
        </h1>
+  <div className="min-h-screen">
+    {/* <div className="gradient-bg-welcome"> */}
+      {/* <Navbar /> */}
+      {/* <Welcome /> */}
+      {/* <Services /> */}
+      {/* <Home /> */}
+      {/* <Transactions /> */}
+      {/* <Client /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    {/* </div> */}
+  {/* </div> */}
     </div>
-  )
+  </div>
+  );
 }
 
 export default App
