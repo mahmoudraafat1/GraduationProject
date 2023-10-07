73cdcf49c87b47a5050fa08ed786c8cdf230cf5b
TheMostafax <mh1546226@gmail.com>
2023-10-07 17:45:52 +0300

    add hardhat config logic and some folders

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,5 +2,32 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: Alchemy_HTTP_KEY,
+      accounts: [MetaMask_Wallet_Private_Key],
+    }
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
