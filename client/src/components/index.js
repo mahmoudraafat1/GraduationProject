925e29ee637575a8a201e43027c9ed30fff28ca2
thisismostafaxx_ <mh1546226@gmail.com>
2023-10-07 17:48:42 +0300

    Add some pages to index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,9 @@
 export { default as Loader } from "./Loader";
 export { default as Navbar } from "./Navbar";
-export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Features } from "./Features";
+//export { default as Details } from "./Details";
+//export { default as Home } from "./Home";
 export { default as Footer } from "./Footer";
 export { default as Services } from "./Services";
 export { default as Transactions } from "./Transactions";
