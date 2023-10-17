d076fae9a5cb337b6e6001b74661c2c9f0c1e388
TheMostafax <mh1546226@gmail.com>
2023-10-17 14:12:47 +0300

    Add index.css schema

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,13 @@
 
-//import { Navbar, Home, Footer, Services, Transactions, Welcome, Client, Contact } from "./components";
-const App =() => {
- 
-  return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-          Real Estate Block Chain
-       </h1>
-  <div className="min-h-screen">
-    {/* <div className="gradient-bg-welcome"> */}
-      {/* <Navbar /> */}
-      {/* <Welcome /> */}
-      {/* <Services /> */}
-      {/* <Home /> */}
-      {/* <Transactions /> */}
-      {/* <Client /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
-    {/* </div> */}
-  {/* </div> */}
-    </div>
-  </div>
-  );
-}
+//import { Navbar, Footer, Services, Transactions, Welcome, Client, Contact, About, Features } from './components';
+//import Chat from './components/ChatIcon';
+import {First} from './components';
 
-export default App
+
+const App = () => (
+  <div>
+   <First/>
+  </div>
+);
+
+export default App;
