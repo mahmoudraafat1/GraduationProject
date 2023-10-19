a540571434fceefaee1805ff0830a90b3de50e65
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2023-10-19 22:42:50 +0300

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -23,27 +23,32 @@ const Signup = () => {
 
   return (
     <div className="signup-form">
-      <h1>Sign Up</h1>
+      <h1 className="text-4xl font-bold">Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Name"
           value={name}
           onChange={(event) => setName(event.target.value)}
+          className="w-full p-3 border border-gray-200 rounded-md mt-5"
         />
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
+          className="w-full p-3 border border-gray-200 rounded-md mt-5"
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          className="w-full p-3 border border-gray-200 rounded-md mt-5"
         />
-        <button type="submit">Sign Up</button>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-md mt-5">
+          Sign Up
+        </button>
       </form>
     </div>
   );
