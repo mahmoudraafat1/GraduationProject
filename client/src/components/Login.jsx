c27d3ec6da22ff35ba4efdcafdbd1b491c4be540
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2023-10-19 22:43:30 +0300

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,21 +22,25 @@ const Login = () => {
 
   return (
     <div className="login-form">
-      <h1>Log In</h1>
+      <h1 className="text-4xl font-bold">Log In</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
+          className="w-full p-3 border border-gray-200 rounded-md mt-5"
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          className="w-full p-3 border border-gray-200 rounded-md mt-5"
         />
-        <button type="submit">Log In</button>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-md mt-5">
+          Log In
+        </button>
       </form>
     </div>
   );
