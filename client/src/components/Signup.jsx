f114d017ab8579d9330b9e1e6f3525dd765fefd2
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2023-10-19 12:07:34 +0300

    Update Signup.jsx

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Send sign up credentials to backend and create user account
+
+    // If sign up is successful, redirect user to home page
+    navigate("/");
+  };
+
+  return (
+    <div className="signup-form">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
