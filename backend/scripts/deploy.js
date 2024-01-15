bbcc77729f0a6bb40548c42fdae6f151098db8ea
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2024-01-15 18:54:27 +0200

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,11 +1,14 @@
 const main = async () => {
   const Transactions = await hre.ethers.deployContract("Transactions");
+  const PropertyRegistration = await hre.ethers.deployContract("PropertyRegistration");
 
   console.log("Deploying, please wait.");
-  
-  await Transactions.waitForDeployment();
 
-  console.log(`Contract deployed to ${await Transactions.getAddress()}`);
+  await Transactions.waitForDeployment();
+  await PropertyRegistration.waitForDeployment();
+
+  console.log(`Transactions contract deployed to ${await Transactions.getAddress()}`);
+  console.log(`PropertyRegistration contract deployed to ${await PropertyRegistration.getAddress()}`);
 };
 
 const runMain = async () => {
