753c36408a44b66769a8451c50fd97ff8c38bbfd
thisismostafaxx_ <mh1546226@gmail.com>
2024-01-19 17:18:20 +0200

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./styless.module.css";
 
+
 const Signup = () => {
 	const [data, setData] = useState({
 		firstName: "",
