8da6c6eedff2e83adff85facf807f1db957489fa
thisismostafaxx_ <mh1546226@gmail.com>
2024-01-19 17:21:47 +0200

    Finished Front end side connected to BlockChain (Hardhat)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
 // eslint-disable-next-line react/no-deprecated
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
+	<BrowserRouter>
+	<App />
+	</BrowserRouter>		
+</React.StrictMode>,
 );
