973dd9704a7dea7a3c67dbce030e8cb5a2b09cf9
thisismostafaxx_ <mh1546226@gmail.com>
2024-01-19 17:18:59 +0200

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -20,3 +20,4 @@ export { default as Error } from "./Error";
 export { default as PageLoader } from "./PageLoader";
 //export { default as PropertyList } from "./PropertyList";
 //export { default as PropertyForm } from "./PropertyForm";
+//export { default as PropertyNewForm } from "./PropertyNewForm";
