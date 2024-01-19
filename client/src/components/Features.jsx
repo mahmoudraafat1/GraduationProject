01a9e55890eccaadc04b6fc40be9dbb610b1cbb9
thisismostafaxx_ <mh1546226@gmail.com>
2024-01-19 14:57:19 +0200

    Update Features.jsx

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -4,8 +4,6 @@ import { propertyList } from "../data/Data";
 import { TransactionContext } from "../context/TransactionContext";
 import Loader from './Loader';
 
-//const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-
 // eslint-disable-next-line react/prop-types
 const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
