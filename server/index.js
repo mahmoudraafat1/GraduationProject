ac51abc3f49295572f3bd69717b1144ae7ccbc30
thisismostafaxx_ <mh1546226@gmail.com>
2024-01-19 15:03:29 +0200

    Finished Server side Express

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+
 // database connection
 connection();
 
