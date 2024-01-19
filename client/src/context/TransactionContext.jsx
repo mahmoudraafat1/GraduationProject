b8681c003c81a569dc921c624fe513c646c2181d
thisismostafaxx_ <mh1546226@gmail.com>
2024-01-19 17:20:00 +0200

    Update TransactionContext.jsx

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-
 import { contractABI, contractAddress } from "../utils/constant";
-
 export const TransactionContext = React.createContext();
-
 const { ethereum } = window;
 
 const createEthereumContract = () => {
