abfd7d1b316f974ee2c3b91145476c064dcdcf7a
Mahmoud Raafat <69508027+mahmoudraafat1@users.noreply.github.com>
2024-06-20 15:18:14 +0300

    updated server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Server side for the Land Property BlockChain is now runinng....');
+  });
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Connecting to server......');
+    console.log(`Server now listening on port ${port}`);
+    console.log(`Now you can run localhost/${port} and see a message`);
+  });
